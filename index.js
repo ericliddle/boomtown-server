1d5d6fd18b1ddcd6ed5aaa5278b23faac99a8f6e
heyliddle <eric.liddle@gmail.com>
2017-07-20 15:23:48 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {
+    graphqlExpress,
+    graphiqlExpress
+} from 'graphql-server-express';
+import cors from 'cors';
+import schema from './api/schema';
+
+const GQL_PORT = 4400;
+const app = express();
+
+app.use('*', cors());
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+}));
+
+app.listen(GQL_PORT, () => console.log(
+    `GraphQL is now running on localhost:${GQL_PORT}/graphiql`
+)); 
